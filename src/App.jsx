9bbe5bde01c545632ad228687f357d3e3604f10c
Eduardo Cavalcante <42215395+Eduardo-Paixao@users.noreply.github.com>
2021-10-05 22:56:59 -0300

    Criação do componente Repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss'
 
 export function App() {
   // throw new Error("Eita pleura, a casa caiu!!!");
-  return <h1>Heyy!!!</h1>;
+  return (<RepositoryList/>);
 }
