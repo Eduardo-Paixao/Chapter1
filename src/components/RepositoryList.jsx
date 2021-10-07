e11912b6b42ce9e17153ba5555d361b53b1594f7
Eduardo Cavalcante <42215395+Eduardo-Paixao@users.noreply.github.com>
2021-10-06 23:37:21 -0300

    criação do componente RepositoryItem e propriedades

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,14 +1,19 @@
-export function RepositoryList(){
-    return(
-        <section>
-            <h1>Lista de repositório</h1>
-            <ul>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar repositório</a>
-                </li>
-            </ul>
-        </section>
-    )
+import { RepositoryItem } from "./RepositoryItem";
+
+const repostory = {
+  name: "uform",
+  description: "Forms inReact",
+  link: "https://github.com/unform/unform",
+};
+export function RepositoryList() {
+  return (
+    <section>
+      <h1>Lista de repositório</h1>
+      <ul>
+        <RepositoryItem repository={repostory} />
+        <RepositoryItem repository={repostory} />
+        <RepositoryItem repository={repostory} />
+      </ul>
+    </section>
+  );
 }
