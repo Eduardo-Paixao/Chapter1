b7183abf83193cd631425173c954757894eeb343
Eduardo Cavalcante <42215395+Eduardo-Paixao@users.noreply.github.com>
2021-10-08 23:32:15 -0300

    instalação e configuração do react-FastRefresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,50 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugi = require("@pmmmwh/react-refresh-webpack-plugin");
 
-const isDEvelopment = process.env.NODE_ENV !== 'production';
+const isDEvelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-    mode: isDEvelopment ? 'development': 'production',
-    devtool: isDEvelopment ? 'eval-source-map' : 'source-map',
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve:{
-        extensions: ['.js', '.jsx'],
-    },
-    devServer:{
-        liveReload: true
-        // contentBase:path.resolve(__dirname, 'public')
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        })
+  mode: isDEvelopment ? "development" : "production",
+  devtool: isDEvelopment ? "eval-source-map" : "source-map",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    liveReload: true,
+    hot: true,
+    // contentBase:path.resolve(__dirname, 'public')
+  },
+  plugins: [
+    isDEvelopment && new ReactRefreshWebpackPlugi(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDEvelopment && require.resolve("react-refresh/babel"),
+            ].filter(Boolean),
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
-    module:{
-        rules:[
-            {
-                test:/\.jsx$/,
-                exclude:/node_modules/,
-                use: 'babel-loader',
-            },
-            {
-                test:/\.scss$/,
-                exclude:/node_modules/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            }
-        ]
-    }
-}
+  },
+};
