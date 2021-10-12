8d98b2c0bbc92550d5b61a7dbf23911c5fcfc580
Eduardo Cavalcante <42215395+Eduardo-Paixao@users.noreply.github.com>
2021-10-11 23:40:58 -0300

    Configurando typescript

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,13 @@ const isDEvelopment = process.env.NODE_ENV !== "production";
 module.exports = {
   mode: isDEvelopment ? "development" : "production",
   devtool: isDEvelopment ? "eval-source-map" : "source-map",
-  entry: path.resolve(__dirname, "src", "index.jsx"),
+  entry: path.resolve(__dirname, "src", "index.tsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   devServer: {
     liveReload: true,
@@ -29,7 +29,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
